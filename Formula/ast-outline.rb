c765238b1df8269357c5d464ca8b589a3579e997
Aero <aero.windwalker@gmail.com>
2026-05-11 00:37:33 +0800

    chore: bump version

--- Formula/ast-outline.rb
+++ Formula/ast-outline.rb
@@ -1,8 +1,8 @@
 class AstOutline < Formula
   desc "Fast AST-based code-navigation toolkit: shape, surface, deps, search"
   homepage "https://github.com/aeroxy/ast-outline"
-  url "https://github.com/aeroxy/ast-outline/releases/download/2.0.1/ast-outline-macos-arm64.zip"
-  sha256 "a76c4e384a0dd155a42b6dc7b2fe4f125de7c5ede04ddb8e7ee8fbab51fc0f34"
+  url "https://github.com/aeroxy/ast-outline/releases/download/2.1.0/ast-outline-macos-arm64.zip"
+  sha256 "c0e3be2da5da9b40f6fc3f3568f6bbe5b7d73224085b7975a9d9fba3b0a2f598"
   license "MIT"
 
   def install
